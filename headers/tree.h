5ad4f778dbf6d14c06d1167601bb68703f36518b
Niladri Das <boney0310@gmail.com>
2023-06-15 00:33:58 +0530

    added code to support Leetcode style tree building using a vector of elements

--- headers/tree.h
+++ headers/tree.h
@@ -39,4 +39,33 @@ public:
                this->right->DFS();
             }
         }
+
+        static TreeNode* makeTree(vector<string>elements)
+        {
+            if(elements.size()==0 || elements[0]=="null")
+            return nullptr;
+            unordered_map<int, TreeNode*>referenceMap;
+            for(int i = 0; i<elements.size(); i++)
+            {
+                if(elements[i]=="null") continue;
+                int n = stoi(elements[i]);
+                if(i==0)
+                referenceMap[i] = new TreeNode(n);
+
+                if((2*i+1) < elements.size() && elements[2*i+1]!="null")
+                {
+                    TreeNode *temp = new TreeNode(stoi(elements[2*i+1]));
+                    referenceMap[i]->left = temp;
+                    referenceMap[2*i+1] = temp;
+                }
+
+                if((2*i+2) < elements.size() && elements[2*i+2]!="null")
+                {
+                    TreeNode *temp = new TreeNode(stoi(elements[2*i+2]));
+                    referenceMap[i]->right = temp;
+                    referenceMap[2*i+2] = temp;
+                }
+            }
+            return referenceMap[0];
+        }
 };
