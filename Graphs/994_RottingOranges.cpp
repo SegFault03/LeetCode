2a7fda765195789f39f0dcf4b0ce70a2d044737f
SegFault03 <boney0310@gmail.com>
2024-01-20 00:59:38 +0530

    updated 994

--- Graphs/994_RottingOranges.cpp
+++ Graphs/994_RottingOranges.cpp
@@ -16,57 +16,56 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size(), orange = 0, maxT = 0;
-        vector<vector<int>>vis(m,vector<int>(n,-1));
-        vector<pair<int,int>>rotten;
-        queue<pair<int,int>>queue;
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                if(grid[i][j]==2)
-                rotten.push_back({i,j});
-                if(grid[i][j]!=0)
-                orange++;
+        vector<vector<int>> vis(m, vector<int>(n, -1));
+        vector<pair<int, int>> rotten;
+        queue<pair<int, int>> queue;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2)
+                    rotten.push_back({i, j});
+                if (grid[i][j] != 0)
+                    orange++;
             }
         }
-        for(int i=0;i<rotten.size();i++) {
+        for (int i = 0; i < rotten.size(); i++) {
             queue.push({rotten[i].first, rotten[i].second});
             vis[rotten[i].first][rotten[i].second] = 0;
-            while(!queue.empty()) {
-                int r = queue.front().first;
-                int c = queue.front().second;
-                queue.pop();
-                if(r+1<m && grid[r+1][c]!=0 && (vis[r+1][c]==-1 || vis[r+1][c]>vis[r][c]+1))
-                {
-                    vis[r+1][c] = vis[r][c]+1;
-                    queue.push({r+1,c});
-                }
-                
-                if(r-1>=0 && grid[r-1][c]!=0 && (vis[r-1][c]==-1 || vis[r-1][c]>vis[r][c]+1))
-                {
-                    vis[r-1][c] = vis[r][c]+1;
-                    queue.push({r-1,c});
-                }
-                
-                if(c+1<n && grid[r][c+1]!=0 && (vis[r][c+1]==-1 || vis[r][c+1]>vis[r][c]+1))
-                {
-                    vis[r][c+1] = vis[r][c]+1;
-                    queue.push({r,c+1});
-                }
-                
-                if(c-1>=0 && grid[r][c-1]!=0 && (vis[r][c-1]==-1 || vis[r][c-1]>vis[r][c]+1))
-                {
-                    vis[r][c-1] = vis[r][c]+1;
-                    queue.push({r,c-1});
-                }
+        }
+        while (!queue.empty()) {
+            int r = queue.front().first;
+            int c = queue.front().second;
+            queue.pop();
+            if (r + 1 < m && grid[r + 1][c] != 0 &&
+                (vis[r + 1][c] == -1 || vis[r + 1][c] > vis[r][c] + 1)) {
+                vis[r + 1][c] = vis[r][c] + 1;
+                queue.push({r + 1, c});
+            }
+
+            if (r - 1 >= 0 && grid[r - 1][c] != 0 &&
+                (vis[r - 1][c] == -1 || vis[r - 1][c] > vis[r][c] + 1)) {
+                vis[r - 1][c] = vis[r][c] + 1;
+                queue.push({r - 1, c});
+            }
+
+            if (c + 1 < n && grid[r][c + 1] != 0 &&
+                (vis[r][c + 1] == -1 || vis[r][c + 1] > vis[r][c] + 1)) {
+                vis[r][c + 1] = vis[r][c] + 1;
+                queue.push({r, c + 1});
+            }
+
+            if (c - 1 >= 0 && grid[r][c - 1] != 0 &&
+                (vis[r][c - 1] == -1 || vis[r][c - 1] > vis[r][c] + 1)) {
+                vis[r][c - 1] = vis[r][c] + 1;
+                queue.push({r, c - 1});
             }
         }
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                if(grid[i][j]!=0)
-                {
-                    if(vis[i][j]==-1)
-                    return -1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] != 0) {
+                    if (vis[i][j] == -1)
+                        return -1;
                     else
-                    maxT = max(maxT, vis[i][j]);
+                        maxT = max(maxT, vis[i][j]);
                 }
             }
         }
